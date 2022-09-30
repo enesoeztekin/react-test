872eaf2d9a3be36d94327d5bdbd65273dbc79b31
Enes Öztekin <enesoztekinofficial@gmail.com>
2022-09-30 19:48:44 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import sass from "./App.scss";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Views/Layout";
+import Home from "./Views/Home";
+import ReactIcons from "./Views/ReactIcons";
+import ReactSlick from "./Views/ReactSlick";
+import Toastify from "./Views/Toastify";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="/react-icons" element={<ReactIcons />} />
+          <Route path="/react-slick" element={<ReactSlick />} />
+          <Route path="/toastify" element={<Toastify />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
